acbb4e5c87247d44d88fbd04ad1408a83722211a
SK Yang <rock5215@yahoo.com.tw>
2020-07-05 17:58:18 +0800

    Java and Kotlin Variable, Data type

--- src/main/kotlin/com/sky/Hello.kt
+++ src/main/kotlin/com/sky/Hello.kt
@@ -5,6 +5,18 @@ fun main() {
 //    Human().hello()
       val h = Human()
       h.hello()
+
+      var age : Int = 19
+      age = 20
+
+    var weight = 66.5f
+//    val name = "SKY"
+    var name : String
+    name = "SKY"
+
+
+
+
 }
 
 class Human{
